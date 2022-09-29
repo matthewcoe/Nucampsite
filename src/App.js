fb7b083e147a24f8fd4e12e48316168dc16baf23
Matthew Coe <matthewcoe001@gmail.com>
2022-09-29 10:57:29 -0600

    Added Several Pages and Nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import React from 'react';
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,7 +11,11 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <CampsitesList/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='contact' element={<ContactPage/>} />
+        <Route path='directory' element={<CampsitesDirectoryPage/>} />
+      </Routes>
       <Footer/>
     </div>
   );
