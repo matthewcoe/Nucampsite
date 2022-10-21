ed83910afac39fbcd41779123948e4c58f518f51
Matthew Coe <matthewcoe001@gmail.com>
2022-10-21 01:00:04 -0600

    Website Finishedgit add .!

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import React from 'react';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
+import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+    dispatch(fetchPartners());
+    dispatch(fetchPromotions());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='contact' element={<ContactPage/>} />
-        <Route path='directory' element={<CampsitesDirectoryPage/>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
         <Route
           path='directory/:campsiteId'
-          element={<CampsiteDetailPage/>}
+          element={<CampsiteDetailPage />}
         />
-        <Route path='about' element={<AboutPage/>}/>
+        <Route path='about' element={<AboutPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
